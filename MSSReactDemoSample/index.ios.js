8ed8b25b6aefeb58e2cadf87ee9fcfeaa1dbd30d
Siddharth <mss.siddharth28@gmail.com>
2017-06-27 15:44:54 +0530

    React demo Application
    React demo Application

---
+++ MSSReactDemoSample/index.ios.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  StatusBar,
+  View
+} from 'react-native';
+import {LoginStack} from './login/loginregisterrouter';
+import {StackNavigator } from 'react-navigation';
+class reactNavigationSample extends Component {
+  render(){
+    return (
+   <LoginStack/>
+    );
+  }
+}
+AppRegistry.registerComponent('MssReactDemo', () => reactNavigationSample);
